4c5b94f062b03e5a403b49085dfe38e52d2176fe
Graciela Contreras <gracielaestefani@gmail.com>
2023-05-17 19:53:02 -0400

    Habilitación de CORS.

--- src/main/java/com/tutorial/crudspringbootmongoback/crud/controller/ProductController.java
+++ src/main/java/com/tutorial/crudspringbootmongoback/crud/controller/ProductController.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/product")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class ProductController {
     @Autowired
     private ProductService productService;
